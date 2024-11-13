92f22a9d12758736e153d51dcf8dcbc1601eb3b3
Anurag Mittal <anurag.mittal@scality.com>
2024-11-13 07:12:07 +0000

    COSI-17: add-dev-container-for-sodi-driver
    - the container sets up minikube, s3 and iam services.

---
+++ .devcontainer/setup-dev-container.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Installing Ginkgo and Gomega..."
+go install github.com/onsi/ginkgo/v2/ginkgo
+go get github.com/onsi/gomega/...
+
+# Create a KIND cluster
+echo "Starting Minikube..."
+minikube start
+
+echo "Logging into GHCR (GitHub Container Registry)..."
+echo "$REPOSITORY_USER_TOKEN" | docker login ghcr.io -u "$REPOSITORY_USER" --password-stdin
+
+# Navigate to the directory and prepare the environment
+echo "Preparing S3 and IAM log and directories..."
+cd .github/s3_and_iam_deployment && \
+mkdir -p logs/s3 logs/iam logs/cosi_driver data/vaultdb && \
+sudo chown -R vscode:vscode logs data && \
+chmod -R ugo+rwx logs data && \
+
+# Start Docker Compose for the 'iam_s3' profile
+echo "Deploying S3 and IAM using docker compose..."
+docker compose --profile iam_s3 up -d
+
+# Set Minikube's Docker environment variables
+eval $(minikube docker-env)
+
+# Prune Docker on Minikube's Docker Daemon
+echo "Pruning unused images..."
+docker system prune -af
+
+echo "Setup complete."
