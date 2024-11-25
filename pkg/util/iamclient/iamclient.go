b8ff68c3698ecd128163ba4175b42994c7c68928
Anurag Mittal <anurag.mittal@scality.com>
2024-11-25 21:10:20 +0100

    COSI-71: add-iam-client-and-associated-tests
    Added a new IAM client with the following functionality. This will be
    used to provide bucket access to end user.
    
    Support for
    - creating users
    - attaching inline policies
    - generating access keys.
    Integration with github.com/aws/aws-sdk-go-v2/service/iam.
    Added iamclient_test.go for unit tests covering all critical operations,
    including error scenarios.
    Changed RunSpecs description for S3Client tests to reflect consistency
    ("S3Client Suite" -> "S3Client Test Suite").
    
    An IAM user is created and the inline policy is restricted to the
    bucket specified. An Access Key ID and Secret Access Key are generated
    for the IAM user.

---
+++ pkg/util/iamclient/iamclient.go
@@ -0,0 +1,192 @@
+package iamclient
+
+import (
+	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+	"github.com/aws/smithy-go/logging"
+	"k8s.io/klog/v2"
+)
+
+type IAMAPI interface {
+	CreateUser(ctx context.Context, input *iam.CreateUserInput, opts ...func(*iam.Options)) (*iam.CreateUserOutput, error)
+	PutUserPolicy(ctx context.Context, input *iam.PutUserPolicyInput, opts ...func(*iam.Options)) (*iam.PutUserPolicyOutput, error)
+	CreateAccessKey(ctx context.Context, input *iam.CreateAccessKeyInput, opts ...func(*iam.Options)) (*iam.CreateAccessKeyOutput, error)
+}
+
+const (
+	defaultRegion  = "us-east-1"
+	requestTimeout = 15 * time.Second
+)
+
+type IAMParams struct {
+	AccessKey string
+	SecretKey string
+	Endpoint  string
+	Region    string
+	TLSCert   []byte // Optional field for TLS certificates
+	Debug     bool
+}
+
+type IAMClient struct {
+	IAMService IAMAPI
+}
+
+func InitIAMClient(params IAMParams) (*IAMClient, error) {
+	if params.AccessKey == "" || params.SecretKey == "" {
+		return nil, fmt.Errorf("AWS credentials are missing")
+	}
+
+	var logger logging.Logger
+	if params.Debug {
+		logger = logging.NewStandardLogger(os.Stdout)
+	} else {
+		logger = nil
+	}
+
+	httpClient := &http.Client{
+		Timeout: requestTimeout,
+	}
+
+	// in the case where endpoint is HTTPS but no certificate is provided, skip TLS validation
+	isHTTPSEndpoint := strings.HasPrefix(params.Endpoint, "https://")
+	skipTLSValidation := isHTTPSEndpoint && len(params.TLSCert) == 0
+	if isHTTPSEndpoint {
+		httpClient.Transport = ConfigureTLSTransport(params.TLSCert, skipTLSValidation)
+	}
+
+	region := params.Region
+	if region == "" {
+		region = defaultRegion
+	}
+
+	ctx := context.Background()
+
+	awsCfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(params.AccessKey, params.SecretKey, "")),
+		config.WithHTTPClient(httpClient),
+		config.WithLogger(logger),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load AWS config: %w", err)
+	}
+
+	iamClient := iam.NewFromConfig(awsCfg, func(o *iam.Options) {
+		o.BaseEndpoint = &params.Endpoint
+	})
+
+	return &IAMClient{
+		IAMService: iamClient,
+	}, nil
+}
+
+func ConfigureTLSTransport(certData []byte, skipTLSValidation bool) *http.Transport {
+	tlsSettings := &tls.Config{
+		MinVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: skipTLSValidation,
+	}
+
+	if len(certData) > 0 {
+		caCertPool := x509.NewCertPool()
+		if ok := caCertPool.AppendCertsFromPEM(certData); !ok {
+			klog.Warning("Failed to append provided cert data to the certificate pool")
+		}
+		tlsSettings.RootCAs = caCertPool
+	}
+
+	return &http.Transport{
+		TLSClientConfig: tlsSettings,
+	}
+}
+
+// CreateUser creates an IAM user with the specified name.
+func (client *IAMClient) CreateUser(ctx context.Context, userName string) error {
+	input := &iam.CreateUserInput{
+		UserName: &userName,
+	}
+
+	_, err := client.IAMService.CreateUser(ctx, input)
+	if err != nil {
+		return fmt.Errorf("failed to create IAM user %s: %w", userName, err)
+	}
+
+	klog.InfoS("IAM user creation succeeded", "user", userName)
+	return nil
+}
+
+// AttachS3WildcardInlinePolicy attaches an inline policy to an IAM user for a specific bucket.
+func (client *IAMClient) AttachS3WildcardInlinePolicy(ctx context.Context, userName, bucketName string) error {
+	policyName := fmt.Sprintf("%s-bucket-access", bucketName)
+	policyDocument := fmt.Sprintf(`{
+		"Version": "2012-10-17",
+		"Statement": [
+			{
+				"Effect": "Allow",
+				"Action": "s3:*",
+				"Resource": [
+					"arn:aws:s3:::%s",
+					"arn:aws:s3:::%s/*"
+				]
+			}
+		]
+	}`, bucketName, bucketName)
+
+	input := &iam.PutUserPolicyInput{
+		UserName:       &userName,
+		PolicyName:     &policyName,
+		PolicyDocument: &policyDocument,
+	}
+
+	_, err := client.IAMService.PutUserPolicy(ctx, input)
+	if err != nil {
+		return fmt.Errorf("failed to attach inline policy to IAM user %s: %w", userName, err)
+	}
+
+	klog.InfoS("Inline policy attachment succeeded", "user", userName, "policyName", policyName)
+	return nil
+}
+
+// CreateAccessKey generates access keys for an IAM user.
+func (client *IAMClient) CreateAccessKey(ctx context.Context, userName string) (*iam.CreateAccessKeyOutput, error) {
+	input := &iam.CreateAccessKeyInput{
+		UserName: &userName,
+	}
+
+	output, err := client.IAMService.CreateAccessKey(ctx, input)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create access key for IAM user %s: %w", userName, err)
+	}
+
+	klog.InfoS("Access key creation succeeded", "user", userName)
+	return output, nil
+}
+
+// CreateBucketAccess is a helper that combines user creation, policy attachment, and access key generation.
+func (client *IAMClient) CreateBucketAccess(ctx context.Context, userName, bucketName string) (*iam.CreateAccessKeyOutput, error) {
+	err := client.CreateUser(ctx, userName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.AttachS3WildcardInlinePolicy(ctx, userName, bucketName)
+	if err != nil {
+		return nil, err
+	}
+
+	accessKeyOutput, err := client.CreateAccessKey(ctx, userName)
+	if err != nil {
+		return nil, err
+	}
+
+	return accessKeyOutput, nil
+}
