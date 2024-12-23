8234a07658f898eb5b55d77e08eef027234f7312
Anurag Mittal <anurag.mittal@scality.com>
2024-12-23 18:52:08 +0100

    metrics tests complete

--- pkg/metrics/metrics_test.go
+++ pkg/metrics/metrics_test.go
@@ -2,17 +2,15 @@ package metrics_test
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"net/http"
-	"strings"
 	"testing"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/scality/cosi-driver/pkg/metrics"
 )
 
 func TestGRPCFactorySuite(t *testing.T) {
@@ -20,97 +18,105 @@ func TestGRPCFactorySuite(t *testing.T) {
 	RunSpecs(t, "Metrics Test Suite")
 }
 
-var _ = Describe("Metrics Server", func() {
+type failingListener struct{}
+
+func (f *failingListener) Accept() (net.Conn, error) {
+	return nil, fmt.Errorf("simulated listener failure")
+}
+
+func (f *failingListener) Close() error {
+	return nil
+}
+
+func (f *failingListener) Addr() net.Addr {
+	return &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 0}
+}
+
+var _ = Describe("Metrics", func() {
 	var (
-		server      *http.Server
-		listener    net.Listener
-		registry    *prometheus.Registry
-		grpcMetrics *prometheus.CounterVec
+		addr              string
+		registry          *prometheus.Registry
+		driverMetricsPath string
 	)
 
 	BeforeEach(func() {
-		// Create a fresh registry
+		addr = "127.0.0.1:0" // Use a random available port
 		registry = prometheus.NewRegistry()
-
-		grpcMetrics = prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Name: "grpc_server_started_total",
-				Help: "Total number of RPCs started on the server.",
-			},
-			[]string{"grpc_method", "grpc_service", "grpc_type"},
-		)
-		Expect(registry.Register(grpcMetrics)).To(Succeed())
-
-		// Increment gRPC metrics to simulate usage
-		grpcMetrics.WithLabelValues("DriverGetInfo", "cosi.v1alpha1.Identity", "unary").Add(5)
-		grpcMetrics.WithLabelValues("DriverCreateBucket", "cosi.v1alpha1.Provisioner", "unary").Add(4)
-		grpcMetrics.WithLabelValues("DriverDeleteBucket", "cosi.v1alpha1.Provisioner", "unary").Add(3)
-		grpcMetrics.WithLabelValues("DriverGrantBucketAccess", "cosi.v1alpha1.Provisioner", "unary").Add(2)
-		grpcMetrics.WithLabelValues("DriverRevokeBucketAccess", "cosi.v1alpha1.Provisioner", "unary").Add(1)
-
-		// Wrap the Prometheus handler with the fresh registry
-		mux := http.NewServeMux()
-		mux.Handle("/metrics", promhttp.HandlerFor(registry, promhttp.HandlerOpts{}))
-
-		// Create and start the metrics server
-		var err error
-		server = &http.Server{Handler: mux}
-		listener, err = net.Listen("tcp", "127.0.0.1:0")
-		Expect(err).NotTo(HaveOccurred())
-
-		go func() {
-			err := server.Serve(listener)
-			if err != nil && err != http.ErrServerClosed {
-				panic(err)
-			}
-		}()
-
-		// Wait for the server to start
-		time.Sleep(100 * time.Millisecond)
+		driverMetricsPath = "/metrics"
 	})
 
-	AfterEach(func() {
-		// Shutdown the server if it is still running
-		if server != nil {
-			err := server.Close()
-			if err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
-				Expect(err).NotTo(HaveOccurred())
-			}
-		}
+	Describe("StartMetricsServerWithRegistry", func() {
+		It("should start a metrics server successfully", func() {
+			server, err := metrics.StartMetricsServerWithRegistry(addr, registry, driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(server).NotTo(BeNil())
 
-		// Close the listener if it's still open
-		if listener != nil {
-			err := listener.Close()
-			if err != nil && !strings.Contains(err.Error(), "use of closed network connection") {
-				Expect(err).NotTo(HaveOccurred())
-			}
-		}
+			resp, err := http.Get("http://" + server.Addr + driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			err = server.Close()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should return an error when the address is invalid", func() {
+			invalidAddr := "invalid:address"
+			server, err := metrics.StartMetricsServerWithRegistry(invalidAddr, registry, driverMetricsPath)
+			Expect(err).To(HaveOccurred())
+			Expect(server).To(BeNil())
+		})
 	})
 
-	It("should expose gRPC metrics on the Prometheus endpoint", func() {
-		addr := listener.Addr().String()
-		resp, err := http.Get(fmt.Sprintf("http://%s%s", addr, "/metrics"))
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	Describe("StartMetricsServerWithListenerAndRegistry", func() {
+		It("should start a metrics server successfully with a custom listener", func() {
+			listener, err := net.Listen("tcp", addr)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(listener).NotTo(BeNil())
 
-		// Read and validate metrics
-		body, err := io.ReadAll(resp.Body)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.Body.Close()).To(Succeed())
-		metricsOutput := string(body)
+			server, err := metrics.StartMetricsServerWithListenerAndRegistry(listener, registry, driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(server).NotTo(BeNil())
 
-		Expect(metricsOutput).To(ContainSubstring(`grpc_server_started_total{grpc_method="DriverGetInfo",grpc_service="cosi.v1alpha1.Identity",grpc_type="unary"} 5`))
-		Expect(metricsOutput).To(ContainSubstring(`grpc_server_started_total{grpc_method="DriverCreateBucket",grpc_service="cosi.v1alpha1.Provisioner",grpc_type="unary"} 4`))
-		Expect(metricsOutput).To(ContainSubstring(`grpc_server_started_total{grpc_method="DriverDeleteBucket",grpc_service="cosi.v1alpha1.Provisioner",grpc_type="unary"} 3`))
-		Expect(metricsOutput).To(ContainSubstring(`grpc_server_started_total{grpc_method="DriverGrantBucketAccess",grpc_service="cosi.v1alpha1.Provisioner",grpc_type="unary"} 2`))
-		Expect(metricsOutput).To(ContainSubstring(`grpc_server_started_total{grpc_method="DriverRevokeBucketAccess",grpc_service="cosi.v1alpha1.Provisioner",grpc_type="unary"} 1`))
-	})
+			resp, err := http.Get("http://" + server.Addr + driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
 
-	It("should return 404 for invalid metrics paths", func() {
-		addr := listener.Addr().String()
-		resp, err := http.Get(fmt.Sprintf("http://%s/invalid-path", addr))
-		Expect(err).NotTo(HaveOccurred())
-		Expect(resp.StatusCode).To(Equal(http.StatusNotFound))
-		Expect(resp.Body.Close()).To(Succeed())
+			err = server.Close()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should serve metrics on the specified path", func() {
+			listener, err := net.Listen("tcp", addr)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(listener).NotTo(BeNil())
+
+			server, err := metrics.StartMetricsServerWithListenerAndRegistry(listener, registry, driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(server).NotTo(BeNil())
+
+			resp, err := http.Get("http://" + server.Addr + driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			resp, err = http.Get("http://" + server.Addr + "/invalid-path")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusNotFound))
+
+			err = server.Close()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should log an error when the listener fails", func() {
+			listener := &failingListener{}
+
+			server, err := metrics.StartMetricsServerWithListenerAndRegistry(listener, registry, driverMetricsPath)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(server).NotTo(BeNil())
+
+			time.Sleep(100 * time.Millisecond)
+
+			err = server.Close()
+			Expect(err).NotTo(HaveOccurred())
+		})
 	})
 })
